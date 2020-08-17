cd3a262a9d45b75476b30e9808e41ebcec1aadfd
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2020-08-17 14:13:19 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
+
 # Update system
-apt update
+sudo apt update && sudo apt upgrade -y
+
 # Install major dependencies
-apt install python3 steamlink
+sudo apt install python3 steamlink
+
 # Install python dependencies
 pip3 install pynput
+
 #Install media chromium
-curl '-fsSL https://pi.vpetkov.net -o ventz-media-pi'
+curl -fsSL https://pi.vpetkov.net -o ventz-media-pi
 sh ventz-media-pi
+
 # Download python script
-mkdir '/home/pi/Documents/smarttv'
-cd '/home/pi/Documents/smarttv'
-wget 'https://raw.githubusercontent.com/jnstockley/raspberrypi-smart-tv/master/app.py'
+mkdir /home/pi/Documents/smarttv
+cd /home/pi/Documents/smarttv
+wget https://raw.githubusercontent.com/jnstockley/raspberrypi-smart-tv/master/app.py
+
 # Auto run pyhton script
-mkdir '/home/pi/.config/autostart' || echo 'Auto start already exists'
-echo '[Desktop Entry]
+mkdir /home/pi/.config/autostart || echo "Auto start already exists"
+echo "[Desktop Entry]
 Type= Application
 Name= Smart TV
-Exec /usr/bin/python3 /home/pi/Documents/smarttv/app.pv' >> /home/pi/.config/autostart/smarttv.desktop
+Exec /usr/bin/python3 /home/pi/Documents/smarttv/app.pv
+" >> /home/pi/.config/autostart/smarttv.desktop
