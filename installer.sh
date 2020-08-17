9c411b54db819eb387b3cc53b5b9b6c144a6ffa8
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2020-08-17 13:56:13 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-
 # Update system
 sudo apt update
-
 # Install major dependencies
 sudo apt install python3 steamlink
-
 # Install python dependencies
 pip3 install pynput
-
 #Install media chromium
 curl -fsSL https://pi.vpetkov.net -o ventz-media-pi
 sh ventz-media-pi
-
 # Download python script
 mkdir /home/pi/Documents/smarttv
 cd /home/pi/Documents/smarttv
 wget https://raw.githubusercontent.com/jnstockley/raspberrypi-smart-tv/master/app.py
-
 # Auto run pyhton script
 mkdir /home/pi/.config/autostart || echo "Auto start already exists"
 echo "[Desktop Entry]
