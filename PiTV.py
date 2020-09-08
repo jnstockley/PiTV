41550784514b2d1ab5027b3d41cdd9c015472973
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2020-09-07 19:56:01 -0500

    Version 0.2

--- PiTV.py
+++ PiTV.py
@@ -13,65 +13,50 @@ app = Flask(__name__)
 #Global variables
 chrome = "chromium-browser"
 flags = '--user-agent="Mozilla/5.0 (X11; CrOS armv7l 12371.89.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"'
-currentVersion = 0.1
+currentVersion = 0.2
 
 # Launches chrome with special flags and searches youtube for request search item
 @app.route('/youtube', methods = ['POST'])
 def youtube():
-    if(update()):
-        return "Please update PiTV!"
-    else:
-        baseURl = "https://www.youtube.com/results?search_query="
-        unprocessed = request.get_json()["url"]
-        processed = unprocessed.replace(" ", "+")
-        url = baseURl + processed
-        subprocess.Popen([chrome, flags, url])
-        return "Searching youtube for " + unprocessed
+    baseURl = "https://www.youtube.com/results?search_query="
+    unprocessed = request.get_json()["url"]
+    processed = unprocessed.replace(" ", "+")
+    url = baseURl + processed
+    subprocess.Popen([chrome, flags, url])
+    return "Searching youtube for " + unprocessed
 
 # Launches chrome with special flags and loads users youtube subscriptions
 @app.route('/subscriptions')
 def subscription():
-    if(update()):
-        return "Please update PiTV!"
-    else:
-        url = "https://www.youtube.com/feed/subscriptions"
-        subprocess.Popen([chrome, flags, url])
-        return "Loading your youtube subscriptions!"
+    url = "https://www.youtube.com/feed/subscriptions"
+    subprocess.Popen([chrome, flags, url])
+    return "Loading your youtube subscriptions!"
 
 # Launches steam link
 @app.route('/steam')
 def steam():
-    if(update()):
-        return "Please update PiTV!"
-    else:
-        subprocess.Popen("steamlink")
-        return "Launching Steam Link!"
+    subprocess.Popen("steamlink")
+    return "Launching Steam Link!"
 
 # Lauches chrome with special flags and loads xfinity stream for live tv
 @app.route('/xfinity', methods = ['POST'])
 def xfinity():
-    if(update()):
-        return "Please update PiTV!"
-    else:
-        baseURL = "https://www.xfinity.com/stream/search?q="
-        unprocessed = request.get_json()["url"]
-        processed = unprocessed.replace(" ", "%20")
-        url = baseURL + processed
-        subprocess.Popen([chrome, flags, url])
-        return "Launching Xfinity Steam. Enjoy " + unprocessed
+    baseURL = "https://www.xfinity.com/stream/search?q="
+    unprocessed = request.get_json()["url"]
+    processed = unprocessed.replace(" ", "%20")
+    url = baseURL + processed
+    subprocess.Popen([chrome, flags, url])
+    return "Launching Xfinity Steam. Enjoy " + unprocessed
 
 # Lauches chrome with special flags and loads justwatch.com to allow the user to select a show/movie to watch
 @app.route('/stream', methods = ['POST'])
 def stream():
-    if(update()):
-        return "Please update PiTV!"
-    else:
-        baseURL = "https://www.justwatch.com/us/search?q="
-        unprocessed = request.get_json()["url"]
-        processed = unprocessed.replace(" ", "%20")
-        url = baseURL + processed
-        subprocess.Popen([chrome, flags, url])
-        return "Searching justwatch.com for " + unprocessed + ". Enjoy!"
+    baseURL = "https://www.justwatch.com/us/search?q="
+    unprocessed = request.get_json()["url"]
+    processed = unprocessed.replace(" ", "%20")
+    url = baseURL + processed
+    subprocess.Popen([chrome, flags, url])
+    return "Searching justwatch.com for " + unprocessed + ". Enjoy!"
 
 # Kills all the tasks that could be launched by PiTV
 @app.route('/quit')
@@ -92,6 +77,14 @@ def update():
         return True
     else:
         return False
+
+# Checks to see if there is a newer version of PiTV on the github repository
+@app.route('/update')
+def test():
+    if(update()):
+        return "Please update PiTV!", 500
+    else:
+        return "Up to date!"
 
 ## Runs the program
 if __name__ == '__main__':
