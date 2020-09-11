ed954421c21cab95ecf047773def4ccf84689a3e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2020-09-10 19:10:25 -0500

    Add files via upload

---
+++ PiTV-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Update System
+sudo apt update && sudo apt upgrade -y
+
+# Change directory into PiTV folder, remove old version, and download new version
+cd /home/pi/Documents/PiTV
+rm PiTV.py
+wget https://raw.githubusercontent.com/jnstockley/PiTV/master/PiTV.py
+cd ~
+echo "PiTV has been updated!"
